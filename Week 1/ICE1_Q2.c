ee1a2d7e5bc2a511c818b14eb634d282e5b42d3b
Pang Jun Rong (Jayden) <61274749+jaydxn1@users.noreply.github.com>
2020-12-16 20:07:42 +0800

    Create ICE1_Q2.c

---
+++ Week 1/ICE1_Q2.c
@@ -0,0 +1,20 @@
+#Solution by Jayden
+
+#include <stdio.h>
+
+int main(void) {
+    int n1;
+    printf("Enter n1:");
+    scanf(" %d", &n1);
+
+    int n2;
+    printf("Enter n2:");
+    scanf(" %d", &n2);
+
+    int sum = n1 + n2;
+    int product = n1 * n2;
+    int quotient = n1 / n2;
+    printf("%d + %d = %d\n", n1, n2, sum);
+    printf("%d * %d = %d\n", n1, n2, product);
+    printf("%d / %d = %d\n", n1, n2, quotient);
+}
